5e20eeb0cf20318dafda93bcd8b35ca61e509054
Kevin <kevinjamesbiju2000@gmail.com>
2024-12-13 12:59:53 +0530

    removed one file

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HeroSection from "../components/HomePageComponents/HeroSection";
 import HowItWorks from "../components/HomePageComponents/HowItWorks";
-import PopularDestinations from "../components/HomePageComponents/PopularDestinations";
 import ScrollToTop from "../components/ScrollToTop";
 
 const HomePage = () => {
@@ -12,7 +11,6 @@ const HomePage = () => {
       <Header />
       <HeroSection />
       <HowItWorks />
-      <PopularDestinations />
       <Faq />
       <Footer />
       <ScrollToTop />
